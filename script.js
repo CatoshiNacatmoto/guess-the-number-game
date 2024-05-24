5be85d09eb7cf4d71a0dd15ea84e54c53b65f478
unknown <catoshinacatmoto@outlook.com>
2024-05-23 20:24:43 -0700

    add game logic

--- script.js
+++ script.js
@@ -7,34 +7,60 @@ const highscore = document.querySelector('.highscore');
 const score = document.querySelector('.score');
 const background = document.querySelector('body');
 const number = document.querySelector('.number');
+const again = document.querySelector('.again');
 
-const correctNum = Math.trunc(Math.random() * 20) + 1;
-
-console.log(correctNum);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let point = 20;
+let topScore = 0;
 
 checkNumber.addEventListener('click', () => {
   const guestNum = +guessInput.value;
-  if (guestNum === correctNum) {
-    //print message if correct
-    message.innerHTML = 'You guess the correct number';
+
+  if (guestNum === secretNumber) {
     //print highscore
-    highscore.innerHTML = point;
+    if (point > topScore) {
+      topScore = point;
+      highscore.textContent = topScore;
+      console.log(topScore);
+    }
     //background turn green
     background.style.backgroundColor = '#60b347';
 
     //show the mystery number
-    number.innerHTML = correctNum;
+    number.innerHTML = secretNumber;
+
+    message.textContent = 'You guess the correct number';
+
+    guessInput.disabled = true;
+    checkNumber.disabled = true;
+  } else if (!guestNum) {
+    message.textContent = 'No Number!';
   } else {
+    //print message if to low or to high
+    message.textContent = guestNum < secretNumber ? 'too low' : 'too high';
     //score -1 after each false guess
     point--;
-
     score.innerHTML = point;
-    //print message if to low or to high
-    message.innerHTML = guestNum < correctNum ? 'too low' : 'too high';
+    if (point === 0) message.textContent = 'You lost';
 
     //reseting the field after fail attempt
     guessInput.value = '';
   }
 });
+
+function playAgain() {
+  //reset the point to 20
+  background.style.backgroundColor = '#222';
+  message.textContent = 'Start Guessing';
+  point = 20;
+  score.innerHTML = point;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  guessInput.value = '';
+
+  guessInput.disabled = false;
+  checkNumber.disabled = false;
+}
+
+// Add the again  functionality
+again.addEventListener('click', playAgain);
